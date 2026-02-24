4bf57e2bdb5964cb13ab019d5915d40f128389bf
Ananya Vasudev <ananyav@andrew.cmu.edu>
2026-02-23 23:14:02 -0600

    updated app code as per mp4 instructions

--- application.py
+++ application.py
@@ -117,9 +117,18 @@ def insert_data_into_db(payload):
     NOTE: Our autograder will automatically insert data into the DB automatically keeping in mind the explained SCHEMA, you dont have to insert your own data.
     """
     create_db_table()
-    # TODO: Implement the database call    
-    
-    raise NotImplementedError("Database insert function not implemented.")
+    connection = get_db_connection()
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                insert_sql = "INSERT INTO events (title, description, image_url, date, location) VALUES (%s, %s, %s, %s, %s)"
+                values = (payload['title'],payload['description'],payload['image_url'],payload['date'],payload['location'])
+                cursor.execute(insert_sql, values)
+            connection.commit()
+            logging.info("Values inserted into table")
+    except Exception as e:
+        logging.exception("Failed to insert values into events table")
+        raise RuntimeError(f"Table insertion failed: {str(e)}")
 
 #Database Function Stub
 def fetch_data_from_db():
@@ -127,9 +136,18 @@ def fetch_data_from_db():
     Stub for database communication.
     Implement this function to fetch your data from the database.
     """
-    # TODO: Implement the database call
-    
-    raise NotImplementedError("Database fetch function not implemented.")
+    connection = get_db_connection()
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                fetch_sql = "SELECT * FROM events ORDER BY date"
+                cursor.execute(fetch_sql)
+                fetched_data = cursor.fetchall()
+                logging.info("Values fetched from table")
+                return(fetched_data)
+    except Exception as e:
+        logging.exception("Failed to fetch values from events table")
+        raise RuntimeError(f"Fetching data failed: {str(e)}")
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
