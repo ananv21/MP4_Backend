5cc9e3e7990defc6534ffead7f84daa5a4cb6c5b
Ananya Vasudev <ananyav@andrew.cmu.edu>
2026-03-01 02:42:48 -0500

    reverting payload read changes

--- application.py
+++ application.py
@@ -122,23 +122,13 @@ def insert_data_into_db(payload):
         with get_db_connection() as connection:
             with connection.cursor() as cursor:
                 insert_sql = "INSERT INTO events (title, description, image_url, date, location) VALUES (%s, %s, %s, %s, %s)"
-                if type(payload)==list:
-                    for item in payload:
-                        values = (item['title'],
-                            item['description'],
-                            item['image_url'],
-                            item['date'],
-                            item['location']
-                            )
-                        cursor.execute(insert_sql, values)
-                else:
-                    values = (payload['title'],
-                            payload['description'],
-                            payload['image_url'],
-                            payload['date'],
-                            payload['location']
-                            )
-                    cursor.execute(insert_sql, values)
+                values = (payload['title'],
+                          payload.get('description',''),
+                          payload.get('image_url',''),
+                          payload['date'],
+                          payload.get('location','')
+                          )
+                cursor.execute(insert_sql, values)
             connection.commit()
             logging.info("Values inserted into table")
     except Exception as e:
